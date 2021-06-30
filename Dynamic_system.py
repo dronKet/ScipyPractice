8da3869e0558bbe3e6548ae8472b9ade79ee0012
Dinar <my@mail.ru>
2021-06-30 18:36:44 +0500

    Build_with_wrong_function

--- Dynamic_system.py
+++ Dynamic_system.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib import pylab as p
 from scipy import integrate
+import math
 
 
 class LotkiVolterra:
@@ -10,6 +11,7 @@ class LotkiVolterra:
         self.b = b
         self.c = c
         self.d = d
+
 
     def dX_dt(self, X, t=0):
         """ Return the growth rate of fox and rabbit populations. """
@@ -26,12 +28,12 @@ class LotkiVolterra:
 
     def analysis(self, t, X0):
         X_f0 = np.array([0., 0.])
-        X_f1=self.X_f1_Func()
-        if all(self.dX_dt(X_f0) != np.zeros(2)) and all(self.dX_dt(X_f1) != np.zeros(2)):
+        self.X_f1=self.X_f1_Func()
+        if all(self.dX_dt(X_f0) != np.zeros(2)) and all(self.dX_dt(self.X_f1) != np.zeros(2)):
              return None
 
         A_f0 = self.d2X_dt2(X_f0)
-        A_f1 = self.d2X_dt2(X_f1)
+        A_f1 = self.d2X_dt2(self.X_f1)
         lambda1, lambda2 = np.linalg.eigvals(A_f1)  # >>> (1.22474j, -1.22474j)
         # They are imaginary numbers. The fox and rabbit populations are periodic as follows from further
         # analysis. Their period is given by:
@@ -59,6 +61,56 @@ class LotkiVolterra:
         p.ylabel('population')
         p.title('Evolution of fox and rabbit populations')
         f1.savefig(name)
+
+    def plot_x_t_phase(self, name='rab_and_fox.png'):
+        t = np.linspace(0, 15, 1000)  # time
+        X0 = np.array([10, 5])  # initials conditions: 10 rabbits and 5 foxes
+        res = self.analysis(t, X0)
+        if res is None:
+            return None
+        rabbits, foxes = res.T
+        values = np.linspace(0.3, 0.9, 5)  # position of X0 between X_f0 and X_f1
+        vcolors = p.cm.autumn_r(np.linspace(0.3, 1., len(values)))  # colors for each trajectory
+        print(values)
+        f2 = p.figure()
+
+        # -------------------------------------------------------
+        # plot trajectories
+        for v, col in zip(values, vcolors):
+            X0 = v * self.X_f1  # starting point
+            X = integrate.odeint(self.dX_dt, X0, t)  # we don't need infodict here
+            p.plot(X[:, 0], X[:, 1], lw=3.5 * v, color=col, label='X0=(%.f, %.f)' % (X0[0], X0[1]))
+
+        # -------------------------------------------------------
+        # define a grid and compute direction at each point
+        ymax = p.ylim(ymin=0)[1]  # get axis limits
+        xmax = p.xlim(xmin=0)[1]
+        nb_points = 20
+
+        x = np.linspace(0, xmax, nb_points)
+        y = np.linspace(0, ymax, nb_points)
+
+        X1, Y1 = np.meshgrid(x, y)  # create a grid
+        DX1, DY1 = self.dX_dt([X1, Y1])  # compute growth rate on the gridt
+        M = (np.hypot(DX1, DY1))  # Norm of the growth rate
+        M[M == 0] = 1.  # Avoid zero division errors
+        DX1 /= M  # Normalize each arrows
+        DY1 /= M
+
+        # -------------------------------------------------------
+        # Drow direction fields, using matplotlib 's quiver function
+        # I choose to plot normalized arrows and to use colors to give information on
+        # the growth speed
+        p.title('Trajectories and direction fields')
+        Q = p.quiver(X1, Y1, DX1, DY1, M, pivot='mid', cmap=p.cm.jet)
+        p.xlabel('Number of rabbits')
+        p.ylabel('Number of foxes')
+        p.legend()
+        p.grid()
+        p.xlim(0, xmax)
+        p.ylim(0, ymax)
+        f2.savefig('rabbits_and_foxes_2.png')
+
 
 
 
@@ -90,7 +142,9 @@ if __name__ == "__main__":
     e = 0.8
     system = LotkiVolterra(a, b, c, d)
     system.plot_x_t('test.png')
-    plt.show()
+   # plt.show()
     modif = LotkiVolterraModified(a, b, c, d,e )
     modif.plot_x_t('test1.png')
+    #plt.show()
+    modif.plot_x_t_phase('test.png')
     plt.show()
